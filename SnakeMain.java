29529a18cba4b781056f9e56c768b8829985009c
spazken <60335118+spazken@users.noreply.github.com>
2021-06-21 19:15:51 -0400

    Create SnakeMain.java

---
+++ SnakeMain.java
@@ -0,0 +1,11 @@
+
+public class SnakeMain {
+    // Constructor for SnakeMain
+	SnakeMain(){}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GameFrame frame = new GameFrame();
+
+	}
+
+}
